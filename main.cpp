6173361bc69a3bdcc330afedf90d5c037f568ac5
svang56 <svang56@mail.csuchico.edu>
2018-04-16 17:05:06 -0700

    done Sorting

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-#include <iostream>
+#include <iostream>//Author: Sunny Vang
+
+
 using namespace std;
+
+void sortingInts(int &, int &, int &);
 
 int main()
 {
@@ -8,7 +12,7 @@ int main()
   cout<<"Enter Red, Green, and Blue values: ";
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
-
+sortingInts(red, green, blue);
 
 
 
@@ -18,4 +22,32 @@ int main()
   cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
   return 0;
   //...END OF "DO NOT CHANGE" AREA
+}
+
+void sortingInts(int &red, int &green, int &blue)
+{
+    int replacement;
+
+    if(red <=  green)
+    {
+        replacement = red;
+
+        red = green;
+
+        green = replacement;
+    }
+
+    if( red <= blue)
+    {
+        replacement = red;
+        red = blue;
+        blue = replacement;
+    }
+
+    if(green <= blue)
+    {
+        replacement = green;
+        green = blue;
+        blue = replacement;
+    }
 }
